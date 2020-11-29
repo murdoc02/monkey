f9ce97dd1104d0ac3e04e25a7adb275e6da3dafb
Richard <garzon.r@gmail.com>
2020-11-29 08:45:15 +0000

    pg 66 register infix parse functions

--- src/parser/parser.go
+++ src/parser/parser.go
@@ -212,3 +212,28 @@ func (p *Parser) parsePrefixExpression() ast.Expression {
 
 	return expression
 }
+
+var precedences = map[token.TokenType]int {
+	token.EQ: EQUALS,
+	token.NOT_EQ: EQUALS,
+	token.LT:	LESSGREATER,
+	token.GT: LESSGREATER,
+	token.PLUS: SUM,
+	token.MINUS: SUM,
+	token.SLASH: PRODUCT,
+	token.ASTERISK: PRODUCT,
+}
+
+func (p *Parser) peekPrecedence() int {
+	if p, ok := precedences[p.peekToken.Type}; ok {
+		return p
+	}
+	return LOWEST
+}
+
+func (p *Parser) curPrecedence() int {
+	if p, ok := precedences[p.peekToken.Type]; ok {
+		return p
+	}
+	return LOWEST
+}
