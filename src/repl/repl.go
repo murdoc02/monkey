13942433794cb0d56c314f341a3f2479587fb7c3
richard garzon <garzon.r@gmail.com>
2021-01-11 14:05:27 -0500

    strategies of evaluation

--- src/repl/repl.go
+++ src/repl/repl.go
@@ -10,6 +10,19 @@ import (
 
 const PROMPT = ">> "
 
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 
@@ -37,7 +50,10 @@ func Start(in io.Reader, out io.Writer) {
 }
 
 func printParserErrors(out io.Writer, errors []string) {
-	for _, msg := range errors {
-		io.WriteString(out, "\t"+msg+"\n")
-	}
+		io.WriteString(out, MONKEY_FACE)
+		io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+		io.WriteString(out, " parser errors:\n")
+		for _, msg := range errors {
+			io.WriteString(out, "\t"+msg+"\n")
+		}
 }
