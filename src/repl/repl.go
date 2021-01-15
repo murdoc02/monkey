127bbe0db6580a5d63804c21a3bf35b1e184c59b
rg <garzon.r@gmail.com>
2021-01-15 09:27:01 -0500

    3.4 representing objects

--- src/repl/repl.go
+++ src/repl/repl.go
@@ -50,10 +50,10 @@ func Start(in io.Reader, out io.Writer) {
 }
 
 func printParserErrors(out io.Writer, errors []string) {
-		io.WriteString(out, MONKEY_FACE)
-		io.WriteString(out, "Woops! We ran into some monkey business here!\n")
-		io.WriteString(out, " parser errors:\n")
-		for _, msg := range errors {
-			io.WriteString(out, "\t"+msg+"\n")
-		}
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
 }
